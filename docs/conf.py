dd915c115895ae6aa8d636c0d7ef13ef2d1b9f9b
Keembar Desta <kebar12desta@gmail.com>
2025-06-19 13:54:51 +0300

    Format code with Black to fix CI/CD check

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,21 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-project = 'ACIS Insurance Risk Analytics'
-copyright = '2025, 10 Academy'
-author = '10 Academy'
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "ACIS Insurance Risk Analytics"
+copyright = "2025, 10 Academy"
+author = "10 Academy"
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static'] 
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
